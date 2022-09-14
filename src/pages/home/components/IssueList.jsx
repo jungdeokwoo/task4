238b835fb40ff5534be9d52bf82cdf0072f165e0
jungdeokwoo <dogsocks@dogsocksui-MacBookPro.local>
2022-09-14 14:04:47 +0900

    [ADD]axios 세팅
    axios세팅 및 적용

--- src/pages/home/components/IssueList.jsx
+++ src/pages/home/components/IssueList.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import IssueItem from './IssueItem'
+import { GithubContext } from 'context/GithubContext'
 
 const IssueList = () => {
+  const IssueLists = useContext(GithubContext)
+
   const issueLists = [1, 2, 3]
-  return issueLists.map(issueList => <IssueItem data={issueList} />)
+  return issueLists.map((issueList, index) => <IssueItem key={index} data={issueList} />)
 }
 
 export default IssueList
